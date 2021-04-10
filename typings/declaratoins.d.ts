b9fc97e4d17926f767b078d529ec9ed4316bc685
Segu_g <sei.rm0307@gmail.com>
2021-04-10 15:22:48 +0900

    add typings to git

---
+++ typings/declaratoins.d.ts
@@ -0,0 +1,10 @@
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export = content;
+}
+
+declare module "*.css";
+
+declare module "*.jpg";
+declare module "*.png";
+declare module "*.svg";
