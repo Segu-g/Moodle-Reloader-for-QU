7a51291bcc789c245eb843b9a11d51daef81d1fc
Segu_g <segusegment@gmail.com>
2021-04-10 13:59:27 +0900

    format Usage and Home in options page

--- src/option/Usage.tsx
+++ src/option/Usage.tsx
@@ -25,35 +25,29 @@ export function Usage() {
                     <span className="text-heading">
                         基本の使い方
                     </span>
-                    <div className="row">
-                        <img src={pic_popup_enrol} style={{ width: "600px" }} />
-                        <div>
-                            コースの登録はMoodleのコースのページから拡張機能のアイコンを選択することで出てくるポップアップから出来ます．<br />
-                            コースのidは
-                            <span>
-                                https://moodle.s.kyushu-u.ac.jp/course/view.php?id=xxxxxx
-                            </span>
-                            のようにコースのurlにあるidが用いられます．<br />
-                            名前はコースの略称等，分かりやすい名前を自由に入れてもらって構いません．
-                        </div>
+                    <img src={pic_popup_enrol} style={{ width: "600px" }} /><br/>
+                    <div>
+                        コースの登録はMoodleのコースのページから拡張機能のアイコンを選択することで出てくるポップアップから出来ます．<br />
+                        コースのidは
+                        <span>
+                            https://moodle.s.kyushu-u.ac.jp/course/view.php?id=xxxxxx
+                        </span>
+                        のようにコースのurlにあるidが用いられます．<br />
+                        名前はコースの略称等，分かりやすい名前を自由に入れてもらって構いません．
                     </div>
-                    <div className="row">
-                        <img src={pic_options_schedule} style={{ width: "600px" }} />
-                        <div>
-                            登録したコースは時間割に設定することが出来ます<br />
-                            右のコース一覧からドラッグして登録したい時限にドロップすることで登録が完了します．<br />
-                            </div>
-                        <div>
-                            登録したコースが開講される日に開かれると開始から1分ほど後に別のタブで自動でリロードされます．<br />
-                            開講する前に自動出席の時間が終わるコースでは出席にならないので気を付けてください．
-                        </div>
+                    <img src={pic_options_schedule} style={{ width: "600px" }} /><br/>
+                    <div>
+                        登録したコースは時間割に設定することが出来ます<br />
+                        右のコース一覧からドラッグして登録したい時限にドロップすることで登録が完了します．<br />
                     </div>
-                    <div className="row">
-                        <img src={pic_content_menu} style={{ width: "600px" }} />
-                        <div>
-                            時間割にコースを設定するとMoodleのメニューに次のコースが表示されるようになります．<br />
-                            また，今日の講義をポップアップから見ることもできます．<br />
-                        </div>
+                    <div>
+                        登録したコースが開講される日に開かれると開始から1分ほど後に別のタブで自動でリロードされます．<br />
+                        開講する前に自動出席の時間が終わるコースでは出席にならないので気を付けてください．
+                    </div>
+                    <img src={pic_content_menu} style={{ width: "600px" }} />
+                    <div>
+                        時間割にコースを設定するとMoodleのメニューに次のコースが表示されるようになります．<br />
+                        また，今日の講義をポップアップから見ることもできます．<br />
                     </div>
                 </div>
             </div>
